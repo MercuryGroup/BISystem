aa41f5daeeb1c71f026c6058856eedf76e28e16e
Problemer <rickard.bremer@gmail.com>
2013-10-23 17:15:30 +0200

    listToString Function

--- ETL/src/loadstocks.erl
+++ ETL/src/loadstocks.erl
@@ -66,40 +66,22 @@ ok;
 
 sendData(_Type, List) ->
 
-%Data = e(List),
-%io:format("~p \n", [Data]).
-
-%[_Symbol|_List1] = StockList, {KeySymbol, ValSymbol} = _Symbol,
-%[_Name|_List2] = _List1, {KeyName, ValName} = _Name,
-%[_Change|_List3] = _List2, {KeyChange, ValChange} = _Change,
-%[_Latest|_List4] = _List3, {KeyLatest, ValLatest} = _Latest,
-%[_Percent|_List5] = _List4, {KeyPercent, ValPercent} = _Percent,
-%[_Volume|_List6] = _List5, {KeyVolume, ValVolume} = _Volume,
-%[_Time|_List7] = _List6, {KeyTime, ValTime} = _Time, 
-%[_openinValue|_List8] = _List7, {KeyOpening, ValueOpening} = _openinValue,
-
-
-%A = lists:append([atom_to_list(KeySymbol)," :"]),
- %<<"Symbol :">>, binary:list_to_bin(ValSymbol)},
-
 	Server = couchbeam:server_connection("localhost", 5984, "", []),
 	{ok, Db} = couchbeam:open_or_create_db(Server, ?DATABASE, []),	
-%	Doc = {[{ binary:list_to_bin(A), binary:list_to_bin(ValSymbol)}, { <<"Name :">>, binary:list_to_bin(ValName)}, {<<"Change :" >>, binary:list_to_bin(ValChange)}, {<<"Latest :">>, binary:list_to_bin(ValLatest)}, {<<"Percent :">>, binary:list_to_bin(ValPercent)}, {<<"Volume :">>, binary:list_to_bin(ValVolume)}, {<<"Time :">>, binary:list_to_bin(ValTime)}, {<<"Opening Value :">>, binary:list_to_bin(ValueOpening) }]},
     
-    Doc = { f(List)},
+    Doc = { listToString(List)},
     
-   io:format("~p",[Doc]),
+	io:format("~p",[Doc]),
     {ok, DocResult} = couchbeam:save_doc(Db, Doc),
     io:format("~p", [DocResult]).
 
 convert([]) ->
 	ok.
 
-f([]) -> []; 
-%	"[" ++ Acc ++ "]";
+listToString([]) -> []; 
 
-f([{Key, Val}|T]) ->
-	[{binary:list_to_bin(atom_to_list(Key)), binary:list_to_bin(Val)}| f(T)]. 
+listToString([{Key, Val}|T]) ->
+	[{binary:list_to_bin(atom_to_list(Key)), binary:list_to_bin(Val)}| listToString(T)]. 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
