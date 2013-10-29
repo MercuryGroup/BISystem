1c86f28622a2970206598a80a3749254af8c32f8
Problemer <rickard.bremer@gmail.com>
2013-10-29 16:07:02 +0100

    added load.erl

---
+++ ETL/src/load.erl
@@ -0,0 +1,109 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% File: loadnyse.erl
+%%% @author Rickard Bremer
+%%% @doc
+%%% Load stock data into our database.
+%%% @end
+%%% Created 14 October 2013 (Monday),09:00 by Rickard Bremer
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-module(loadstocks).
+-export([start/0, stop/0, init/0, loop/0, sendData/2]).
+-include("../include/ETL.hrl").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Start and register the process.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-spec start() -> {ok, pid()}.
+start() ->
+	case whereis(?LOAD) of
+		undefined ->
+			register(?LOAD, spawn(?MODULE, init, [])),
+				{ok, whereis(?LOAD)};
+		_ ->
+			{ok, whereis(?LOAD)}
+	end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Stop the process.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+stop() ->
+	case whereis(?LOAD) of
+		undefined ->
+			already_stopped;
+		_ -> 
+			exit(whereis(?LOAD), ok),
+			stopped 
+	end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Start all sub applications and enter the loop.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+init() ->
+	
+	application:start(sasl),
+	application:start(ibrowse),
+	application:start(asn1),
+	application:start(crypto),
+	application:start(public_key),
+	application:start(ssl),
+	application:start(couchbeam),
+	couchbeam:start(),
+
+	loop().	
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Upload the Bin to the database.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sendData(_, []) ->
+ok;
+
+sendData(_Type, List) ->
+	
+	Server = couchbeam:server_connection("localhost", 5984, "", []),
+		{ok, Db} = couchbeam:open_or_create_db(Server, ?DATABASE, []),	
+    
+    Doc = { listToBin(List)},
+ 		{ok, DocResult} = couchbeam:save_doc(Db, Doc),
+    
+    io:format("~p", [DocResult]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Turn List into a Bin.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+listToBin([]) -> []; 
+
+listToBin([{Key, Val}|T]) ->
+	[{unicode:characters_to_binary(atom_to_list(Key)), unicode:characters_to_binary(Val)}| listToBin(T)]. 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Receive a List from the Transform module.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+loop() ->
+	receive 
+		{stock , List} ->
+			sendData(stock, List),
+			loop();
+		{market , List} ->
+			sendData(market, List),
+			loop();
+		List ->
+			io:format("~p~n load ~n", [List]),
+        	%sendData(news, List),
+        	loop()
+
+    end,
+    loop().
