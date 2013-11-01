dd4f93278ed0f713e2e2c74ab823fc347caf0cd3
Joel Berghé <joel.berghe@gmail.com>
2013-11-01 10:13:03 +0100

    Form update

--- desktopapp/BIS Desktop/BIS Desktop/Form1.Designer.cs
+++ desktopapp/BIS Desktop/BIS Desktop/Form1.Designer.cs
@@ -29,55 +29,88 @@
         private void InitializeComponent()
         {
             this.menu = new System.Windows.Forms.Panel();
+            this.marketButton = new System.Windows.Forms.Button();
             this.settings = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.rightPanel = new System.Windows.Forms.Panel();
+            this.stocksButton = new System.Windows.Forms.Button();
+            this.menu.SuspendLayout();
             this.SuspendLayout();
             // 
             // menu
             // 
             this.menu.BackColor = System.Drawing.Color.White;
-            this.menu.Location = new System.Drawing.Point(12, 12);
+            this.menu.Controls.Add(this.stocksButton);
+            this.menu.Controls.Add(this.marketButton);
+            this.menu.Location = new System.Drawing.Point(4, 35);
             this.menu.Name = "menu";
-            this.menu.Size = new System.Drawing.Size(230, 657);
+            this.menu.Size = new System.Drawing.Size(168, 634);
             this.menu.TabIndex = 0;
             this.menu.Paint += new System.Windows.Forms.PaintEventHandler(this.menu_Paint);
             // 
+            // marketButton
+            // 
+            this.marketButton.BackColor = System.Drawing.Color.LightGray;
+            this.marketButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.marketButton.Location = new System.Drawing.Point(0, 100);
+            this.marketButton.Name = "marketButton";
+            this.marketButton.Size = new System.Drawing.Size(154, 82);
+            this.marketButton.TabIndex = 0;
+            this.marketButton.Text = "Market";
+            this.marketButton.UseVisualStyleBackColor = false;
+            this.marketButton.Click += new System.EventHandler(this.marketButton_Click);
+            // 
             // settings
             // 
-            this.settings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.settings.Location = new System.Drawing.Point(248, 12);
+            this.settings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.settings.Location = new System.Drawing.Point(178, 35);
             this.settings.Name = "settings";
-            this.settings.Size = new System.Drawing.Size(497, 51);
+            this.settings.Size = new System.Drawing.Size(567, 51);
             this.settings.TabIndex = 1;
             // 
-            // panel3
+            // leftPanel
             // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.panel3.Location = new System.Drawing.Point(248, 69);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(497, 600);
-            this.panel3.TabIndex = 2;
+            this.leftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.leftPanel.Location = new System.Drawing.Point(178, 92);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(567, 577);
+            this.leftPanel.TabIndex = 2;
             // 
-            // panel4
+            // rightPanel
             // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.panel4.Location = new System.Drawing.Point(751, 12);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(501, 657);
-            this.panel4.TabIndex = 3;
+            this.rightPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.rightPanel.Location = new System.Drawing.Point(751, 35);
+            this.rightPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(511, 634);
+            this.rightPanel.TabIndex = 3;
+            // 
+            // stocksButton
+            // 
+            this.stocksButton.BackColor = System.Drawing.Color.LightGray;
+            this.stocksButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stocksButton.Location = new System.Drawing.Point(0, 182);
+            this.stocksButton.Name = "stocksButton";
+            this.stocksButton.Size = new System.Drawing.Size(154, 82);
+            this.stocksButton.TabIndex = 0;
+            this.stocksButton.Text = "Stocks";
+            this.stocksButton.UseVisualStyleBackColor = false;
+            this.stocksButton.Click += new System.EventHandler(this.stocksButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.rightPanel);
             this.Controls.Add(this.settings);
             this.Controls.Add(this.menu);
+            this.Controls.Add(this.leftPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(1);
             this.Text = "Form1";
+            this.menu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -86,8 +119,10 @@
 
         private System.Windows.Forms.Panel menu;
         private System.Windows.Forms.Panel settings;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Panel rightPanel;
+        private System.Windows.Forms.Button marketButton;
+        private System.Windows.Forms.Button stocksButton;
     }
 }
 
--- desktopapp/BIS Desktop/BIS Desktop/Form1.cs
+++ desktopapp/BIS Desktop/BIS Desktop/Form1.cs
@@ -12,8 +12,13 @@ namespace BIS_Desktop
 {
     public partial class Form1 : Form
     {
+        private Boolean marketClicked, stocksClicked;
+        Class1 leftPanelValue;
         public Form1()
         {
+            marketClicked = false;
+            stocksClicked = false;
+            leftPanelValue = new Class1();
             InitializeComponent();
         }
 
@@ -21,14 +26,64 @@ namespace BIS_Desktop
         {
 
         }
-        
-            
-    }
-    public class Temp : Panel
+        private void menuClick(String button, Boolean toggled)
         {
-            public Temp()
+            Color buttonColor = Color.LightGray;
+            if (!toggled)
             {
-                
+                resetMenuButtons();
+                buttonColor = Color.CornflowerBlue;
+                leftPanel.Controls.Add(leftPanelValue);
+                leftPanelValue.Size = leftPanel.Size;
+                //Temp
+
+            }
+            if(leftPanel.Contains(leftPanelValue))
+            {
+                leftPanel.Controls.Remove(leftPanelValue);
+            }
+            leftPanel.Controls.Add(leftPanelValue);
+            switch (button)
+            {
+                case "market":
+                    marketButton.BackColor = buttonColor;
+                    break;
+                case "stocks":
+                    stocksButton.BackColor = buttonColor;
+                    break;
+                case "news":
+                    break;
+                case "portfolio":
+                    break;
+                default:
+                    break;
             }
         }
+        private void resetMenuButtons()
+        {
+            marketClicked = false;
+            stocksClicked = false;
+            marketButton.BackColor = Color.LightGray;
+            stocksButton.BackColor = Color.LightGray;
+        }
+
+        private void stocksButton_Click(object sender, EventArgs e)
+        {
+            
+            if (!stocksClicked)
+            {
+                menuClick("stocks", stocksClicked);
+                stocksClicked = true;
+            }
+        }
+        private void marketButton_Click(object sender, EventArgs e)
+        {
+            
+            if (!marketClicked)
+            {
+                menuClick("market", marketClicked);
+                marketClicked = true;
+            }
+        }
+    }
 }
