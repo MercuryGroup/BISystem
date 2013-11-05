a090f5b810420db74f038ed7bc16fd7a9d9951ad
TankMasterRL <tankmaster93@gmail.com>
2013-11-05 12:00:35 +0100

    Web service API test
    Small/non-important changes in the news feed extractor.

---
+++ webservice/JAXRS-BISystem/src/main/java/com/merc/webservice/rest/jersey/JAXRS_BISystem/News.java
@@ -0,0 +1,29 @@
+package com.merc.webservice.rest.jersey.JAXRS_BISystem;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "news" path)
+ */
+@Path("/news")
+@Produces(MediaType.APPLICATION_JSON)
+public class News {
+    /**
+     * Returns the available news for "symbol".
+     *
+     * @return String that will be returned as a application/json response.
+     */
+    @GET
+    @Path("{symbol}")
+    public String getAvailableNewsData(@PathParam("symbol") String symbol) {
+    	List<String> symbols = Arrays.asList(symbol.split(","));
+        return "{\"NewsData\":" + symbols.toString() + "}";
+    }
+}
