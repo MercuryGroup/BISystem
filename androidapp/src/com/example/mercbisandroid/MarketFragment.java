0d982d052f8996ba170085e716cb6192784e66f7
Problemer <rickard.bremer@gmail.com>
2013-11-19 15:39:07 +0100

    Android list

--- androidapp/src/com/example/mercbisandroid/MarketFragment.java
+++ androidapp/src/com/example/mercbisandroid/MarketFragment.java
@@ -1,26 +1,100 @@
 package com.example.mercbisandroid;
 
+//import com.example.list_fruit.R;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONObject;
+
+import android.support.v4.app.ListFragment;
+
+import android.os.AsyncTask;
+//import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.TextView;
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
  * 
  */
-public class MarketFragment extends Fragment {
+public class MarketFragment extends ListFragment {
+	
+	static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+		"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+		"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
 
+//AsyncTask<ArrayList<Object>, Void, ArrayList<Object>> STOCKS = new StockThread().execute();
+	 
 	public MarketFragment() {
 		// Required empty public constructor
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_market, container, false);
+	public void onActivityCreated(Bundle savedInstanceState) {
+	
+		
+	//	@SuppressWarnings("unchecked")
+		System.out.println("test2");
+		
+	 //  ArrayList<ArrayList> AL = new ArrayList();
+	   
+		
+		//System.out.println("Lista " + Lista.get().);
+		
+	//	JSONObject JOBJ = new JSONObject();
+		
+		//JOBJ.getJSONObject(Lista.get(1));
+		
+		
+		
+		//ArrayList<Object> Test = STOCKS.doInBackground(null);
+		
+		
+		     
+			setListAdapter(new ArrayAdapter<String>(getActivity(), R.layout.list_fruit,FRUITS));
+		    ListView listView = getListView(); //EX: 
+		    listView.setTextFilterEnabled(true);
+		    registerForContextMenu(listView);
+		    super.onActivityCreated(savedInstanceState);
+		
+	}
+	
+	//@Override
+	
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		//public void onCreate(Bundle savedInstanceState) {
+	//		super.onCreate(savedInstanceState);
+	 
+			// no more this
+			// setContentView(R.layout.list_fruit);
+	 
+			//setListAdapter(new ArrayAdapter<String>(getActivity(), R.layout.list_fruit,FRUITS));
+	 
+			//ListView listView = getListView();
+			//listView.setTextFilterEnabled(true);
+	 
+			//listView.setOnItemClickListener(new OnItemClickListener() {
+			//	public void onItemClick(AdapterView<?> parent, View view,
+			//			int position, long id) {
+				    // When clicked, show a toast with the TextView text
+			//	    Toast.makeText(getActivity().getApplicationContext(),((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			//	}
+		//	});
+		//	return 
+	 
+		//}
+	 
+		
 	}
 
-}
+
