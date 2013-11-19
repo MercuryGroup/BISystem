8669522fdcb447d2d703dbaa8e5e5607b7a140a1
Alexander <A.Ask1991@gmail.com>
2013-11-19 15:32:01 +0100

    One big load

---
+++ desktopapp/BIS Desktop/BIS Desktop/News.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIS_Desktop
+{
+    class News
+    {
+
+        public String Id { get; set; }
+
+        public String Key { get; set; }
+
+        public String title { get; set; }
+
+        public String link { get; set; }
+
+        public String description { get; set; } 
+
+        public String guid { get; set; }
+
+        public String pubDate { get; set; }
+
+        public String type { get; set; }
+    }
+}
