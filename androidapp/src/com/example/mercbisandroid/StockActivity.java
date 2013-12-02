59b6dc527a04b6da928f3c79fd3df920938e315f
Problemer <rickard.bremer@gmail.com>
2013-12-02 14:59:51 +0100

    added AChart

--- androidapp/src/com/example/mercbisandroid/StockActivity.java
+++ androidapp/src/com/example/mercbisandroid/StockActivity.java
@@ -1,22 +1,78 @@
 package com.example.mercbisandroid;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import org.afree.chart.ChartFactory;
+import org.afree.chart.plot.PlotOrientation;
+import org.afree.data.category.CategoryDataset;
+import org.afree.data.category.DefaultCategoryDataset;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 public class StockActivity extends Activity {
 	
-	AsyncTask<ArrayList<Object>, Void, ArrayList<Object>> DetailedStockArray;
+	public AsyncTask<ArrayList<Object>, Void, ArrayList<Object>> DetailedStockArray;
 	
 	@Override
-    public void onCreate(Bundle savedInstanceState) {         
-
+    public void onCreate(Bundle savedInstanceState) {   
        super.onCreate(savedInstanceState);    
        setContentView(R.layout.activity_stock);
        DetailedStockArray = new DetailedStockThread().execute();
-       //rest of the code
+     
+       CategoryDataset dataset = CreateDataSet(DetailedStockArray);
+       ViewGroup viewGroup = (ViewGroup)getWindow().getDecorView().findViewById(android.R.id.content);
+       ChartView chartView = new ChartView(this);
+       chartView.drawChart(ChartFactory.createBarChart( "Boink","Category","Value",dataset,PlotOrientation.VERTICAL,true,true,false));       
+
+       viewGroup.addView(chartView);
+       
    }
+
+public static CategoryDataset CreateDataSet(AsyncTask<ArrayList<Object>, Void, ArrayList<Object>> detailedStockArray){
+	
+	DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	
+	try {
+		System.out.println(detailedStockArray.get().size());
+	} catch (InterruptedException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	} catch (ExecutionException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	
+	try {
+		for(int i = 0; i < detailedStockArray.get().size(); i++){
+		 
+		JSONObject JOBJ = new JSONObject(detailedStockArray.get().get(i).toString());
+		
+		String latest = JOBJ.getString("latest");
+		String updated = JOBJ.getString("updated");
+		
+		dataset.addValue(Integer.parseInt(latest), "Value", updated);
+		}
+	} catch (NumberFormatException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (ExecutionException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (JSONException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return dataset; 
+}
+
 
 }
